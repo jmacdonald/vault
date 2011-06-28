a2284272ab2c17ce684b061c3bd53c4ab8bd10a7
Jordan Macdonald <prg04itd@4WTWQB1.(none)>
2011-06-28 16:33:45 -0400

    Re-implementing test methods from scratch.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,27 @@
-var vault;
-var road;
+var mustang, evo, cars;
 $(document).ready(function() {
     var urls = {
         list: "test.json"
     }
-    vault = new Vault('roads', urls, "id", {offline: true});
-    vault.reload(function() {
-        road = vault.fetch(1304);
-        road.name = 'test!';
-        console.log(road);
+    cars = new Vault('cars', urls, {after_load: function() {
 
-        road.delete();
-    });
+        // Add a Viper.
+        cars.add({
+            make: "Dodge",
+            model: "Viper SRT-10",
+            year: 2008
+        });
+
+        // Update the Evo's year.
+        evo = cars.fetch(2);
+        evo.year = 2011;
+        evo.update();
+
+        // Get rid of the Mustang.
+        mustang = cars.fetch(1);
+        mustang.delete();
+
+        // Log the cars to the console.
+        console.log(cars.objects);
+    }});
 });
